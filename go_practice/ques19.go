69cb7d1f0ce06cb00a9a2245a3d80c2df4324516
Rohan Kumar <merohansharma6@gmail.com>
2023-01-09 16:25:31 +0530

    ques19 correction

--- go_practice/ques19.go
+++ go_practice/ques19.go
@@ -8,29 +8,26 @@ import (
 //Write a function that takes a string and returns a new string with all the words in reverse order.
 //Input: "This is a test" Output: "test a is This"
 
-// func StringReversed(str string) string {
-//reversing the string using temp variable to swap the n and len(string)-n till we reach the mid
-//reversing each character
-// strLength := len(str)
-// mid := strLength / 2
-// var temp string = ""
-// for i := 0; i < mid; i++ {
-// 	temp = string(str[i])
-// 	str[i] = str[strLength-i-1]
-// 	str[strLength-i-1] = temp
-// }
-// return temp
+func StringReversed(str string) string {
+	//reversing the string using temp variable to swap the n and len(string)-n till we reach the mid
+	//reversing each character
+	// strLength := len(str)
+	// mid := strLength / 2
+	// var temp string = ""
+	// for i := 0; i < mid; i++ {
+	// 	temp = string(str[i])
+	// 	str[i] = str[strLength-i-1]
+	// 	str[strLength-i-1] = temp
+	// }
+	// return temp
 
-func PrintBackwards(s string) string {
-	part := strings.Split(s, " ")
+	tempString := strings.Split(str, " ")
 	res := []string{}
-	for i := len(part) - 1; i >= 0; i-- {
-		if part[i] == " " {
-			continue
-		}
-		res = append(res, part[i])
+	for i := len(tempString) - 1; i >= 0; i-- {
+		res = append(res, tempString[i])
 	}
 	return strings.Join(res, " ")
+
 }
 
 func main() {
